eb07beb0028a83718d68c93bb1c47b659dc8182c
TheManWhoCodesStuffSometimes <167024529+TheManWhoCodesStuffSometimes@users.noreply.github.com>
2025-08-29 18:14:49 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '../styles/globals.css'
+import { FinanceProvider } from '../context/FinanceContext'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <FinanceProvider>
+      <Component {...pageProps} />
+    </FinanceProvider>
+  )
 }
